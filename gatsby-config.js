559dff1919474adeed05914aa62b2e07bfcbe9dd
HishmatRai <hishmatrai.2018@gmail.com>
2020-01-25 07:14:18 +0500

    Gatsby-Boilerplate

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,8 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-material-ui`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,13 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-
+import '@fortawesome/fontawesome-free/css/all.min.css'; 
+import'bootstrap-css-only/css/bootstrap.min.css';
+ import'mdbreact/dist/css/mdb.css';
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>Hishmat Rai</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
